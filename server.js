f5ad7670c69c9f140f1e73a778f855c49bf9268e
Matt Lee <matthewlee07@gmail.com>
2019-03-02 17:12:00 -0500

    updated package and server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const router = express.Router();
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+app.use(morgan("common"));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
